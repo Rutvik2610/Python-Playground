92dad0f541cb165408c7d3a64df6314ae163e7e3
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-09-13 00:19:03 +0530

    Update coffee_machine.py

--- Coffee Machine/coffee_machine.py
+++ Coffee Machine/coffee_machine.py
@@ -1,4 +1,5 @@
 from machine_data import MENU, resources
+import math
 
 
 def use_resources(menu_item, available_resources):
@@ -10,40 +11,50 @@ def use_resources(menu_item, available_resources):
     avail_milk = available_resources["milk"]
     avail_coffee = available_resources["coffee"]
 
-    if (avail_water > req_water) and (avail_milk > req_milk) and (avail_coffee > req_coffee):
-        avail_water -= req_water
-        avail_milk -= req_milk
-        avail_coffee -= req_coffee
+    if avail_water > req_water:
+        if avail_milk > req_milk:
+            if avail_coffee > req_coffee:
+                available_resources["water"] -= req_water
+                available_resources["milk"] -= req_milk
+                available_resources["coffee"] -= req_coffee
+                print(f"Here is your {menu_item}. Enjoy!")
+            else:
+                print("Sorry there is not enough Coffee")
+        else:
+            print("Sorry there is not enough Milk")
+    else:
+        print("Sorry there is not enough Water")
 
     return resources
 
 
 def use_money(quarters, dimes, nickles, pennies, menu_item):
     money_required = MENU[menu_item]["cost"]
-    money_paid = (quarters*0.25) + (dimes*0.1) + (nickles*0.05) + (pennies*0.01)
+    money_paid = (quarters * 0.25) + (dimes * 0.1) + (nickles * 0.05) + (pennies * 0.01)
 
-    if money_required > money_paid:
-        print(f"Here is ${money_paid-money_required} change.")
+    if money_required < money_paid:
+        total_change = round((money_paid - money_required), 2)
+        print(f"Here is ${money_paid - money_required} change.")
         return money_required
+    else:
+        print("Sorry that's not enough money. Money refunded")
+        return 0
 
 
-def start_coffee_machine():
+money = 0
+use_coffee_machine = True
+while use_coffee_machine:
+    user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    if user_input == "report":
+        print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}\nMoney: ${money}")
+    elif user_input == "exit":
+        use_coffee_machine = False
+    else:
+        print("Please insert coins")
+        quarter = int(input("How many quarters?: "))
+        dime = int(input("How many dimes?: "))
+        nickle = int(input("How many nickles?: "))
+        penny = int(input("How many pennies?: "))
 
-    money = 0
-    use_coffee_machine = True
-    while use_coffee_machine:
-        user_input = input("What would you like? (espresso/latte/cappuccino): ").lower()
-        if user_input == "report":
-            print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}")
-        else:
-            print("Please insert coins")
-            quarter = int(input("How many quarters?: "))
-            dime = int(input("How many dimes?: "))
-            nickle = int(input("How many nickles?: "))
-            penny = int(input("How many pennies?: "))
-
-            money += (quarter, dime, nickle, penny, user_input)
-            resources = use_resources(user_input, resources)
-
-
-start_coffee_machine()
+        money += use_money(quarter, dime, nickle, penny, user_input)
+        resources = use_resources(user_input, resources)
