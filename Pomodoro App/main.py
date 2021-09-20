066b4d3e1988ca679e39871c7172ad623ebc6154
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-09-20 22:30:49 +0530

    Update main.py

--- Pomodoro App/main.py
+++ Pomodoro App/main.py
@@ -1,9 +1,9 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+RED = "#FF2442"
+BEIGE = "#FFEDDA"
+ORANGE = "#FFB830"
+BLUE = "#3DB2FF"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -16,3 +16,14 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=BEIGE)
+
+canvas = Canvas(width=200, height=224, bg=BEIGE, highlightthickness=0)
+tomato_png = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_png)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
+
+window.mainloop()
