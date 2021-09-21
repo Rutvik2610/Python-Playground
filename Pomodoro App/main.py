5ab3433a5e27fb9b6db148e23a985bc397554ea0
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-09-21 15:35:27 +0530

    Update main.py

--- Pomodoro App/main.py
+++ Pomodoro App/main.py
@@ -10,7 +10,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-CHECK_MARK = "✔"
+CHECK_MARK = "🍅"
 reps = 0
 timer = None
 
@@ -20,7 +20,7 @@ timer = None
 
 def reset_timer():
     global reps
-    reps = 0
+    reps += 1
     window.after_cancel(timer)
     start_button.config(state="normal")
     timer_label.config(text="Timer", fg=GREEN)
@@ -31,15 +31,18 @@ def reset_timer():
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
 def start_timer():
+    global reps
+    if start_button["state"] != "disabled":
+        reps = 0
+
     start_button.config(state="disabled")
     window.attributes('-topmost', 1)
     window.attributes('-topmost', 0)
-    global reps
-    reps = 0
     reps += 1
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
+
 
     if reps % 2 != 0:
         count_down(work_sec)
