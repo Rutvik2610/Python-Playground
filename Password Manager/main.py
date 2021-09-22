c95d3319bab337510941de34349dfc5c43e42406
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-09-22 18:21:31 +0530

    Update main.py

--- Password Manager/main.py
+++ Password Manager/main.py
@@ -1,13 +1,38 @@
 from tkinter import *
 from tkinter import messagebox
-
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+
+def generate_password():
+    password_entry.delete(0, END)
+    password = []
+
+    password += [random.choice(letters) for _ in range(nr_letters)]
+    password += [random.choice(symbols) for _ in range(nr_symbols)]
+    password += [random.choice(numbers) for _ in range(nr_numbers)]
+
+    random.shuffle(password)
+    final_password = "".join(password)
+
+    password_entry.insert(0, final_password)
+    window.clipboard_clear()
+    window.clipboard_append(final_password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
-    print(website)
     email = user_entry.get()
     password = password_entry.get()
 
@@ -23,7 +48,6 @@ def save():
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
                 website_entry.focus()
-
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -59,8 +83,8 @@ password_entry = Entry()
 password_entry.grid(row=3, column=1, sticky=EW, padx=5, pady=5)
 
 # Buttons
-generate_password = Button(text="Generate Password")
-generate_password.grid(row=3, column=2, padx=5, pady=5)
+generate_password_button = Button(text="Generate Password", command=generate_password)
+generate_password_button.grid(row=3, column=2, padx=5, pady=5)
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky=EW, padx=5, pady=5)
