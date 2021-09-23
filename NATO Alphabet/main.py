3938280d77e2c2772612625a286d36daa93d1174
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-09-23 19:14:51 +0530

    Update main.py

--- NATO Alphabet/main.py
+++ NATO Alphabet/main.py
@@ -6,10 +6,16 @@ df = pandas.DataFrame(data)
 nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 print(nato_dict)
 
-user_input = input("Enter a word: ").upper()
-try:
-    code_word = [nato_dict[letter] for letter in user_input]
-except KeyError:
-    print("Sorry only letters in the alphabet please")
-else:
-    print(code_word)
+
+def generate_phonetic():
+    user_input = input("Enter a word: ").upper()
+    try:
+        code_word = [nato_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(code_word)
+
+
+generate_phonetic()
