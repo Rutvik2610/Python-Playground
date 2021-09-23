3f6c5e5463b838cf36fde3245b90cc78445ebe09
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-09-23 19:07:41 +0530

    Update main.py

--- NATO Alphabet/main.py
+++ NATO Alphabet/main.py
@@ -7,5 +7,9 @@ nato_dict = {row.letter: row.code for (index, row) in df.iterrows()}
 print(nato_dict)
 
 user_input = input("Enter a word: ").upper()
-code_word = [nato_dict[letter] for letter in user_input]
-print(code_word)
+try:
+    code_word = [nato_dict[letter] for letter in user_input]
+except KeyError:
+    print("Sorry only letters in the alphabet please")
+else:
+    print(code_word)
