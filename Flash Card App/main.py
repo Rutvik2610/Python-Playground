a70250634dd405d49d9d9f59b903f9a94d2b22b5
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-09-25 19:11:33 +0530

    Update main.py

--- Flash Card App/main.py
+++ Flash Card App/main.py
@@ -1,5 +1,8 @@
 from tkinter import *
-# import pandas
+import pandas
+
+data = pandas.read_csv("data/french_words.csv")
+df = pandas.DataFrame(data)
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -14,6 +17,8 @@ back_png = PhotoImage(file="images/card_back.png")
 right_png = PhotoImage(file="images/right.png")
 wrong_png = PhotoImage(file="images/wrong.png")
 canvas.create_image(400, 263, image=front_png)
+canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 263, text=df.French[0], font=("Ariel", 60, "bold"))
 canvas.grid(row=0, column=0, columnspan=2)
 
 # Buttons
@@ -24,4 +29,3 @@ right_button = Button(image=right_png, highlightthickness=0)
 right_button.grid(row=1, column=1)
 
 window.mainloop()
-
