4cf77a82731bddcf2b72ec8e8914854356b515e5
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-09-27 21:23:45 +0530

    Update main.py

--- Automated Birthday Wisher/main.py
+++ Automated Birthday Wisher/main.py
@@ -1,2 +1,26 @@
 import smtplib
 import datetime as dt
+import random
+import pandas
+
+my_email = ""
+password = ""
+
+letter_list = ["letter_1.txt", "letter_2.txt", "letter_3.txt"]
+now = dt.datetime.now()
+today = (now.month, now.day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row.month, data_row.day): data_row.to_list() for (index, data_row) in data.iterrows()}
+print(birthdays_dict)
+
+if today in birthdays_dict:
+    with open(f"letter_templates/{random.choice(letter_list)}") as mail_letter:
+        birthday_letter = mail_letter.read()
+        birthday_letter = birthday_letter.replace("[NAME]", f"{birthdays_dict[today][0]}")
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email,
+                                to_addrs=f"{birthdays_dict[today][1]}",
+                                msg=f"Subject:Happy Birthday\n\n{birthday_letter}")
