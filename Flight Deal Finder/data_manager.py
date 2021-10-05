25844e353529eab4c78cfd769cd17b90ec16f4f1
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-10-05 20:34:25 +0530

    Add files via upload

---
+++ Flight Deal Finder/data_manager.py
@@ -0,0 +1,44 @@
+import os
+import requests
+
+
+USERNAME = os.environ.get("SHEETY_USERNAME")
+TOKEN = os.environ.get("SHEETY_TOKEN")
+
+class DataManager:
+
+    def __init__(self, project_name, sheet_name):
+        self.username = USERNAME
+        self.token = TOKEN
+        self.header = sheety_headers = {
+            "Authorization": f"Bearer {self.token}"
+        }
+        self.project_name = project_name
+        self.sheet_name = sheet_name
+        self.endpoint = f"https://api.sheety.co/{self.username}/{self.project_name}/{self.sheet_name}"
+        self.sheet_data = {}
+
+    def get_data(self):
+        sheet_data = requests.get(url=self.endpoint, headers=self.header)
+        sheet_data.raise_for_status()
+        return sheet_data.json()["prices"]
+
+    def enter_data(self, city_name, lowest_price):
+        prices = {
+            "price": {
+                "city": city_name,
+                "iataCode": "",
+                "lowestPrice": lowest_price
+            }
+        }
+        enter_response = requests.post(url=self.endpoint, json=prices, headers=self.header)
+        enter_response.raise_for_status()
+
+    def edit_data(self, iata_code, row_id):
+        prices = {
+            "price": {
+                "iataCode": iata_code,
+            }
+        }
+        edit_response = requests.put(url=f"{self.endpoint}/{row_id}", json=prices, headers=self.header)
+        edit_response.raise_for_status()
