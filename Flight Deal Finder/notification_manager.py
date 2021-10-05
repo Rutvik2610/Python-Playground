d5fc386299f0b2755a595a02cf1241e73efed127
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-10-05 21:53:40 +0530

    Update notification_manager.py

--- Flight Deal Finder/notification_manager.py
+++ Flight Deal Finder/notification_manager.py
@@ -1,3 +1,23 @@
+import smtplib
+import os
+
+EMAIL_ID = os.environ.get("email_id")
+PASSWORD = os.environ.get("password")
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self, flight_details):
+        self.flight_details = flight_details
+
+    def send_mail(self):
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=EMAIL_ID, password=PASSWORD)
+            connection.sendmail(from_addr=EMAIL_ID,
+                                to_addrs=EMAIL_ID,
+                                msg=f"Low Price Alert!✈\n\n"
+                                    f"Only {self.flight_details.price} to fly from"
+                                    f"{self.flight_details.origin_city}-{self.flight_details.origin_airport} to"
+                                    f"{self.flight_details.destination_city}-{self.flight_details.destination_airport},"
+                                    f"from {self.flight_details.out_date} - "
+                                    f"{self.flight_details.return_date}.".encode("utf8"))
