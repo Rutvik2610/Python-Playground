2072c2dea1abb349f2b837eaecaf0821d5660b54
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-10-05 21:53:00 +0530

    Update main.py

--- Flight Deal Finder/main.py
+++ Flight Deal Finder/main.py
@@ -7,20 +7,28 @@ from notification_manager import NotificationManager
 ORIGIN_CITY_IATA = "BOM"
 
 sheet1 = DataManager("flightDeals", "prices")
-flight1 = FlightSearch()
+flight_search = FlightSearch()
 
 sheet_data = sheet1.get_data()
 # for row in sheet_data["prices"]:
 #     if row["iataCode"] == "":
-#         sheet1.edit_data(flight1.get_city_code(row["city"]), row["id"])
+#         sheet1.edit_data(flight_search.get_city_code(row["city"]), row["id"])
 
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
 for destination in sheet_data:
-    flight = flight1.check_flights(
+    flight = flight_search.check_flights(
         ORIGIN_CITY_IATA,
         destination["iataCode"],
         from_time=tomorrow,
         to_time=six_month_from_today
     )
+    try:
+        price = flight.price
+    except AttributeError:
+        continue
+    else:
+        if flight.price <= destination["lowestPrice"]:
+            notification = NotificationManager(flight)
+            notification.send_mail()
