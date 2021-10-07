7416312f58d3c218dd0aa8b91ebe4d5e6107954a
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-10-07 22:50:13 +0530

    Update main.py

--- Flight Deal Finder/main.py
+++ Flight Deal Finder/main.py
@@ -7,17 +7,20 @@ from notification_manager import NotificationManager
 ORIGIN_CITY_IATA = "BOM"
 
 sheet1 = DataManager("flightDeals", "prices")
+sheet2 = DataManager("flightDeals", "users")
 flight_search = FlightSearch()
 
-sheet_data = sheet1.get_data()
-# for row in sheet_data["prices"]:
+price_sheet_data = sheet1.get_data()
+user_sheet_data = sheet2.get_data()
+user_email_list = [user["email"] for user in user_sheet_data]
+# for row in price_sheet_data["prices"]:
 #     if row["iataCode"] == "":
 #         sheet1.edit_data(flight_search.get_city_code(row["city"]), row["id"])
 
 tomorrow = datetime.now() + timedelta(days=1)
 six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
 
-for destination in sheet_data:
+for destination in price_sheet_data:
     flight = flight_search.check_flights(
         ORIGIN_CITY_IATA,
         destination["iataCode"],
@@ -30,5 +33,5 @@ for destination in sheet_data:
         continue
     else:
         if flight.price <= destination["lowestPrice"]:
-            notification = NotificationManager(flight)
+            notification = NotificationManager(flight, user_email_list)
             notification.send_mail()
