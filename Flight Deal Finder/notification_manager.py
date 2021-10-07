f24a419ea526f3f287f15811b591b8278347bba4
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-10-07 22:52:03 +0530

    Update notification_manager.py

--- Flight Deal Finder/notification_manager.py
+++ Flight Deal Finder/notification_manager.py
@@ -6,18 +6,24 @@ PASSWORD = os.environ.get("password")
 
 
 class NotificationManager:
-    def __init__(self, flight_details):
+    def __init__(self, flight_details, mail_list):
         self.flight_details = flight_details
+        self.mail_list = mail_list
 
     def send_mail(self):
         with smtplib.SMTP("smtp.gmail.com") as connection:
             connection.starttls()
             connection.login(user=EMAIL_ID, password=PASSWORD)
-            connection.sendmail(from_addr=EMAIL_ID,
-                                to_addrs=EMAIL_ID,
-                                msg=f"Low Price Alert!✈\n\n"
-                                    f"Only {self.flight_details.price} to fly from"
-                                    f"{self.flight_details.origin_city}-{self.flight_details.origin_airport} to"
-                                    f"{self.flight_details.destination_city}-{self.flight_details.destination_airport},"
-                                    f"from {self.flight_details.out_date} - "
-                                    f"{self.flight_details.return_date}.".encode("utf8"))
+            for email_id in self.mail_list:
+                connection.sendmail(from_addr=EMAIL_ID,
+                                    to_addrs=email_id,
+                                    msg=f"Subject:Low Price Alert!✈\n\n"
+                                        f"Only {self.flight_details.price} to fly from "
+                                        f"{self.flight_details.origin_city}-{self.flight_details.origin_airport} to"
+                                        f"{self.flight_details.destination_city}-{self.flight_details.destination_airport},"
+                                        f"from {self.flight_details.out_date} - "
+                                        f"{self.flight_details.return_date}.\n"
+                                        f"https://www.google.co.in/flights?hl=en#flt="
+                                        f"{self.flight_details.origin_airport}.{self.flight_details.destination_airport}"
+                                        f".{self.flight_details.out_date}*{self.flight_details.destination_airport}."
+                                        f"{self.flight_details.origin_airport}.{self.flight_details.return_date}".encode("utf8"))
