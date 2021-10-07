06a90517973e9e1b8a7b18e9dd5c7e54e25f04bd
Rutvik Purohit <70328972+Rutvik2610@users.noreply.github.com>
2021-10-07 22:49:28 +0530

    Update flight_search.py

--- Flight Deal Finder/flight_search.py
+++ Flight Deal Finder/flight_search.py
@@ -52,7 +52,9 @@ class FlightSearch:
             destination_city=data["route"][0]["cityTo"],
             destination_airport=data["route"][0]["flyTo"],
             out_date=data["route"][0]["local_departure"].split("T")[0],
-            return_date=data["route"][1]["local_departure"].split("T")[0]
+            return_date=data["route"][1]["local_departure"].split("T")[0],
+            stop_overs=1,
+            via_city=data["route"][0]["cityTo"]
         )
         print(f"{flight_data.destination_city}: £{flight_data.price}")
         return flight_data
